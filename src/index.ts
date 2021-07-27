ef2b5b5baaded9f6abb07659cae0bff74a3fe21f
JesonOU <zichengou@gmail.com>
2021-07-27 20:54:38 +0800

    basic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+// Create a class for the element
+class Oplayer extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Create a shadow root
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    // Create spans
+    const wrapper = document.createElement('span');
+    wrapper.setAttribute('class', 'wrapper');
+
+    const icon = document.createElement('span');
+    icon.setAttribute('class', 'icon');
+    icon.setAttribute('tabindex', '0');
+
+    const info = document.createElement('span');
+    info.setAttribute('class', 'info');
+
+    // Take attribute content and put it inside the info span
+    const text = this.getAttribute('data-text');
+    info.textContent = text;
+
+    // Insert icon
+    let imgUrl;
+    if (this.hasAttribute('img')) {
+      imgUrl = this.getAttribute('img');
+    } else {
+      imgUrl = 'img/default.png';
+    }
+
+    const img = document.createElement('img');
+    img.setAttribute('src', imgUrl as string);
+    icon.appendChild(img);
+
+    // Create some CSS to apply to the shadow dom
+    const style = document.createElement('style');
+    console.log(style.isConnected);
+
+    style.setAttribute('link', './style.css');
+
+    // Attach the created elements to the shadow dom
+    shadow.appendChild(style);
+    console.log(style.isConnected);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(icon);
+    wrapper.appendChild(info);
   }
-  return a + b;
-};
+}
+
+// Define the new element
+customElements.define('o-player', Oplayer);
